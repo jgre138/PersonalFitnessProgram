cd1da40ea77d6e366e3860985554dc09e496b863
JGre1381886 <s1381886@monmouth.edu>
2025-10-24 14:11:37 -0400

    Add files via upload

--- Project5-s1381886.cpp
+++ Project5-s1381886.cpp
@@ -1,5 +1,5 @@
 /*
-	Project 4 by Jennifer Greene
+	Project 5 by Jennifer Greene
 
 	This is a personal fitness application that records your name, age, gender, height, and the day's weight.
 	Given this information, it will calculate your BMI.The user can log the exercise they did for the day 
